17313c472de2550d1ef51c9ec2d3386a5636e350
alexpanzetta <132580450+alexpanzetta@users.noreply.github.com>
2024-10-23 12:38:37 -0400

    Added ASB Certificates scripts

---
+++ Powershell Scripts/Check-ASB-Certificates.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    This script Will check ArchestrA Service Bus (a.k.a ASB) certificates expiration
+
+.NOTES   
+    Name       : Check-ASB-Certificates.ps1
+    Author     : Alex Panzetta
+    Email      : alex.panzetta@aveva.com
+    Version    : 1.0
+    LastModified: October 2024
+#>
+
+Get-ChildItem -Path Cert:\LocalMachine\My | where { $_.Issuer -like "*ASB*" }  |
+  ForEach-Object {
+    $cert = $_
+    $CertificateName = $($cert.FriendlyName)
+    $CertificateExpirationDate = $($cert.NotAfter)
+    $TodaysDate = Get-Date -Format "M/d/yyyy hh:mm:ss tt"
+    $CertTimeDiff = New-TimeSpan -Start $TodaysDate -End $CertificateExpirationDate
+    
+    if ($CertTimeDiff -lt 1) {
+        Write-Host -ForegroundColor Red "[" $CertificateName"] expired on " $CertificateExpirationDate 
+        }
+    else {
+        Write-Host -ForegroundColor Green "[" $CertificateName"] is valid and will expire in " $CertTimeDiff " days" 
+        }
+  }
---
+++ Powershell Scripts/Get-ASB-Certificates.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+    This script Will retrieve ArchestrA Service Bus (a.k.a ASB) certificates use by 
+    Platform Common Services (a.k.a PCS)
+
+.NOTES   
+    Name       : Get-ASB-Certificates.ps1
+    Author     : Alex Panzetta
+    Email      : alex.panzetta@aveva.com
+    Version    : 1.0
+    LastModified: October 2024
+#>
+
+# Load the required assembly
+Add-Type -AssemblyName System.Windows.Forms
+
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+
+# Create a SaveFileDialog object
+$saveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+
+# Set properties for the dialog
+$saveFileDialog.Filter = "All files (*.txt)|*.txt"
+$saveFileDialog.Title = "Save As"
+$saveFileDialog.InitialDirectory = $DesktopPath  
+$saveFileDialog.FileName = $env:COMPUTERNAME + "_ASBCertificates.txt"
+
+# Show the dialog
+$result = $saveFileDialog.ShowDialog()
+
+# Check if the user clicked OK
+if ($result -eq "OK") {
+    # Get the selected file path
+    $outputFilePath = $saveFileDialog.FileName
+    }
+else {
+    Write-Host "Cannot continue without a file path"
+    Exit
+    }
